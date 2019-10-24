60d928c728d794fc1f78f8a7b6030fb4b8ce481d
287178790@qq.com <dongxuwei@dongxuwei-2.local>
2019-10-24 17:32:41 +0800

    q

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-empty */
 import Vue from 'vue'
 
 import Cookies from 'js-cookie'
@@ -19,6 +20,9 @@ import './utils/error-log' // error log
 
 import * as filters from './filters' // global filters
 
+import { urls } from '@/utils/url.js'
+Vue.prototype.urls = urls
+
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
@@ -31,6 +35,39 @@ import { mockXHR } from '../mock'
 if (process.env.NODE_ENV === 'production') {
   mockXHR()
 }
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+// 返回数据拦截处理
+axios.interceptors.response.use(response => {
+  if (response.data) {
+    if (response.data.errcode === '3') {
+      // console.log('未登录,重新登录');
+      // v.$alert('会话过期请重新登录', '提示', {
+      //   confirmButtonText: '确定',
+      //   callback: action => {
+      //     store.commit("userinfo", null);
+      //     removeUserinfo();
+      //   }
+      // });
+      // return;
+    } else if (response.data.errcode === '0') {
+      return {
+        code: 0,
+        msg: response.data.errmsg
+      }
+    } else {
+      return {
+        code: 1,
+        data: response.data.data[0]
+      }
+    }
+  } else {
+  }
+}, error => Promise.reject(error.response))
+// axios.defaults.withCredentials = true;
+// axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8;application/json;';
+Vue.use(VueAxios, axios)
 
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium' // set element-ui default size
